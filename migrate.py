b23331957e83d0c9158b20fd87a00696dc18a587
Osaetin Daniel <osaetindaniel@gmail.com>
2017-01-13 23:34:16 +0100

    Add proper path for votr.py file

--- migrate.py
+++ migrate.py
@@ -1,4 +1,4 @@
 import os
 print('Running migrations if any...')
-os.system('export FLASK_APP=votr.py && flask db stamp &&\
+os.system('export FLASK_APP=Votr/votr.py && flask db stamp &&\
           flask db migrate && flask db upgrade')
